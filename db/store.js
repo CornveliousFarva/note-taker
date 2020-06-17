cce82adec8f04c2d7d8474b30922b3929c14a61a
Greg Schuman <gcschuman@hotmail.com>
2020-06-16 19:18:35 -0700

    Added getNotes and potential scenarios if certain results do not show up.

--- db/store.js
+++ db/store.js
@@ -19,6 +19,17 @@ class Store {
     getNotes(){
         return this.read().then(notes => {
             let parsedNotes;
-        })
+
+            // If notes isn't an array or can't be turned into one, send back a new empty array
+            try {
+                parsedNotes = [].concat(JSON.parse(notes));
+              } catch (err) {
+                parsedNotes = [];
+              }
+        
+              return parsedNotes;
+        });
     }
+
+    
 }
