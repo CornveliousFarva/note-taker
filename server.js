fb8c68e2929ddeb5a0d4dbce17e2ef52e66e781e
Greg Schuman <gcschuman@hotmail.com>
2020-06-16 20:26:17 -0700

    Start the server on the port.

--- server.js
+++ server.js
@@ -12,3 +12,6 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 app.use("/api", apiRoutes);
 app.use("/", htmlRoutes);
+
+// Start the server on the port
+app.listen(PORT, () => console.log(`Listening on PORT: ${PORT}`));
