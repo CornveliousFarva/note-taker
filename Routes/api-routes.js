46212fdb8c94e031a5b0b6addcac3a4a96b665a6
Greg Schuman <gcschuman@hotmail.com>
2020-06-16 20:14:30 -0700

    Added delete to respond with all notes from the database.

--- Routes/api-routes.js
+++ Routes/api-routes.js
@@ -8,11 +8,22 @@ router.get("/notes", function(req, res) {
     .then(notes => res.json(notes))
     .catch(err => res.status(500).json(err));
 });
-
+// POST "/api/notes" responds with all notes from the database
 router.post("/notes", (req, res) => {
   store
     .addNote(req.body)
     .then((note) => res.json(note))
     .catch(err => res.status(500).json(err));
 });
+
+// DELETE "/api/notes" deletes the note with an id equal to req.params.id
+router.delete("/notes/:id", function(req, res) {
+  store
+    .removeNote(req.params.id)
+    .then(() => res.json({ ok: true }))
+    .catch(err => res.status(500).json(err));
+});
+
+module.exports = router;
+
 
