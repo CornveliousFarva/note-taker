b924fd8ff148416357b10d31517b873b760b637c
Greg Schuman <gcschuman@hotmail.com>
2020-06-16 20:21:22 -0700

    Added so all the other routes respond with the index.html file.

--- Routes/html-routes.js
+++ Routes/html-routes.js
@@ -5,3 +5,11 @@ var router = require("express").Router();
 router.get("/notes", function(req, res) {
   res.sendFile(path.join(__dirname, "../public/notes.html"));
 });
+
+// All other routes respond with the index.html file
+router.get("*", function(req, res) {
+  res.sendFile(path.join(__dirname, "../public/index.html"));
+});
+
+module.exports = router;
+
