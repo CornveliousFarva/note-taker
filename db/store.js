e866d3eb6a49c6896b0e318801372cdbd00cee77
Greg Schuman <gcschuman@hotmail.com>
2020-06-16 19:08:37 -0700

    Added initial const declarations.

---
+++ db/store.js
@@ -0,0 +1,8 @@
+const util = require("util");
+const fs = require("fs");
+
+// This package will be used to generate our unique ids.
+const uuidv1 = require('uuid/v1');
+
+const readFileAsync = util.promisify(fs.readFile);
+const writeFileAsync = util.promisify(fs.writeFile);
