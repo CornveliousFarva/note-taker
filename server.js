b3753e89a0433c0aebbdce09f7fa2cdbafb6f03a
Greg Schuman <gcschuman@hotmail.com>
2020-06-16 20:25:21 -0700

    Set up body parsing, static, and route middleware.

--- server.js
+++ server.js
@@ -5,3 +5,10 @@ const htmlRoutes = require("./routes/htmlRoutes");
 // Initialize the app and create a port
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+// Set up body parsing, static, and route middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+app.use("/api", apiRoutes);
+app.use("/", htmlRoutes);
