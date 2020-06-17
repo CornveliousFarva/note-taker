b0be2bb6712d1e88cd65fd8c26126fbbe30e0c31
Greg Schuman <gcschuman@hotmail.com>
2020-06-16 20:19:24 -0700

    Added the /notes response so it responds to the notes.html file.

--- Routes/html-routes.js
+++ Routes/html-routes.js
@@ -1,2 +1,7 @@
 var path = require("path");
 var router = require("express").Router();
+
+// "/notes" responds with the notes.html file
+router.get("/notes", function(req, res) {
+  res.sendFile(path.join(__dirname, "../public/notes.html"));
+});
